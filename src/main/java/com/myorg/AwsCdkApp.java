7301d10f80b4f8788d1d388128f9388c360c80bf
heycristhian <heycristhian@gmail.com>
2023-03-06 17:55:17 -0300

    Criado Cluster ECS

--- src/main/java/com/myorg/AwsCdkApp.java
+++ src/main/java/com/myorg/AwsCdkApp.java
@@ -3,6 +3,7 @@ package com.myorg;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ecs.Cluster;
 
 import java.util.Arrays;
 
@@ -10,7 +11,12 @@ public class AwsCdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new VpcStack(app, "Vpc");
+        // CREATING VPC
+        VpcStack vpcStack = new VpcStack(app, "Vpc");
+
+        //CREATING CLUSTER
+        ClusterStack clusterStack = new ClusterStack(app, "Cluster", vpcStack.getVpc());
+        clusterStack.addDependency(vpcStack);
 
         app.synth();
     }
---
+++ src/main/java/com/myorg/ClusterStack.java
@@ -0,0 +1,25 @@
+package com.myorg;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ClusterProps;
+import software.constructs.Construct;
+
+public class ClusterStack extends Stack {
+    public ClusterStack(final Construct scope, final String id, Vpc vpc) {
+        this(scope, id, null, vpc);
+    }
+
+    public ClusterStack(final Construct scope, final String id, final StackProps props, Vpc vpc) {
+        super(scope, id, props);
+
+        new Cluster(this, "cluster01",
+                ClusterProps.builder()
+                        .clusterName("cluster-01")
+                        .vpc(vpc)
+                        .build()
+        );
+    }
+}
